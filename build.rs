fce5e6bee0b6f976e9d7946d7c2b29ea304fe990
Piper McCorkle <contact@piperswe.me>
2021-08-20 20:29:51 -0500

    Update website

--- build.rs
+++ build.rs
@@ -7,13 +7,15 @@ extern crate cbindgen;
 use std::env;
 
 fn main() {
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let profile = env::var("PROFILE").unwrap();
+    if env::var("CARGO_FEATURE_C").is_ok() {
+        let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+        let profile = env::var("PROFILE").unwrap();
 
-    cbindgen::Builder::new()
-        .with_crate(crate_dir)
-        .with_language(cbindgen::Language::C)
-        .generate()
-        .expect("Unable to generate bindings")
-        .write_to_file(format!("target/{}/datom.h", profile));
+        cbindgen::Builder::new()
+            .with_crate(crate_dir)
+            .with_language(cbindgen::Language::C)
+            .generate()
+            .expect("Unable to generate bindings")
+            .write_to_file(format!("target/{}/datom.h", profile));
+    }
 }
