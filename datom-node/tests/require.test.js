c7828cf8c9f588f5234d60f36c72f923098820dc
Piper McCorkle <contact@piperswe.me>
2021-08-25 00:50:48 -0500

    Add a skeleton for Node bindings

--- datom-node/tests/require.test.js
+++ datom-node/tests/require.test.js
@@ -1,8 +1,7 @@
 // SPDX-FileCopyrightText: 2021 Lutris Engineering, Inc
 // SPDX-License-Identifier: BlueOak-1.0.0 OR BSD-2-Clause-Patent
 // SPDX-FileContributor: Piper McCorkle <piper@lutris.engineering>
-{
-    "rust-analyzer.checkOnSave.command": "clippy",
-    "files.insertFinalNewline": true,
-    "editor.formatOnSave": true
-}
+
+test('can require the module', () => {
+    require('..');
+});
