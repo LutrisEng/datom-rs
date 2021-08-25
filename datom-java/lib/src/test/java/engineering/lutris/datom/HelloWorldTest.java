d0ff525052ec709e08b9a92eaa8343b00bcf7b47
Piper McCorkle <contact@piperswe.me>
2021-08-25 01:58:30 -0500

    Please the REUSE gods

--- datom-java/lib/src/test/java/engineering/lutris/datom/HelloWorldTest.java
+++ datom-java/lib/src/test/java/engineering/lutris/datom/HelloWorldTest.java
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2021 Lutris Engineering, Inc
 // SPDX-License-Identifier: BlueOak-1.0.0 OR BSD-2-Clause-Patent
 // SPDX-FileContributor: Piper McCorkle <piper@lutris.engineering>
+
 package engineering.lutris.datom;
 
 import org.junit.jupiter.api.Test;
