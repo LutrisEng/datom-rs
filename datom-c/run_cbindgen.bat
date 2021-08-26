31dcc7cfda695052c32ed020e20603a98fc33406
Piper McCorkle <contact@piperswe.me>
2021-08-25 21:51:29 -0500

    Appease the REUSE gods

--- datom-c/run_cbindgen.bat
+++ datom-c/run_cbindgen.bat
@@ -1,1 +1,5 @@
+@rem SPDX-FileCopyrightText: 2021 Lutris Engineering, Inc
+@rem SPDX-License-Identifier: BlueOak-1.0.0 OR BSD-2-Clause-Patent
+@rem SPDX-FileContributor: Piper McCorkle <piper@lutris.engineering>
+
 cbindgen --config cbindgen.toml --crate datom-c --output datom.h
