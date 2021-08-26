9034410312bfb9fbdd2b798797427ba866505c78
Piper McCorkle <contact@piperswe.me>
2021-08-26 16:43:07 -0500

    Add tiered storage

--- datom/src/merge_iters.rs
+++ datom/src/merge_iters.rs
@@ -11,38 +11,79 @@ use datom::MergeIters;
 let first: [u64; 4] = [1, 76, 2754, 53326];
 let second: [u64; 3] = [53, 62, 431];
 let third: [u64; 3] = [63235, 5322663, 634463436];
-let mut iters = [first.iter().cloned(), second.iter().cloned(), third.iter().cloned()];
-let merged = MergeIters::new(&mut iters);
-let results: Vec<u64> = merged.collect();
+let iters = vec![first.iter().cloned(), second.iter().cloned(), third.iter().cloned()];
+let merged = MergeIters::new(iters);
+let results: Vec<u64> = merged.map(|x| x.0).collect();
 assert_eq!(vec![1, 53, 62, 76, 431, 2754, 53326, 63235, 5322663, 634463436], results);
 ```
 */
-pub struct MergeIters<'i, T: Ord, I: Iterator<Item = T>> {
-    iterators: &'i mut [I],
-    map: BTreeMap<T, usize>,
+pub struct MergeIters<T: Ord, I: Iterator<Item = T>> {
+    iterators: Vec<I>,
+    front_map: Option<BTreeMap<T, usize>>,
+    back_map: Option<BTreeMap<T, usize>>,
 }
 
-impl<'i, T: Ord, I: Iterator<Item = T>> Iterator for MergeIters<'i, T, I> {
-    type Item = T;
+impl<T: Ord, I: Iterator<Item = T>> Iterator for MergeIters<T, I> {
+    type Item = (T, usize);
 
     fn next(&mut self) -> Option<Self::Item> {
-        let (val, from_iterator) = self.map.pop_first()?;
+        let map = if let Some(map) = &mut self.front_map {
+            map
+        } else {
+            let mut map = BTreeMap::new();
+            for (i, iterator) in self.iterators.iter_mut().enumerate() {
+                if let Some(v) = iterator.next() {
+                    map.insert(v, i);
+                }
+            }
+            self.front_map = Some(map);
+            if let Some(map) = &mut self.front_map {
+                map
+            } else {
+                panic!()
+            }
+        };
+        let (val, from_iterator) = map.pop_first()?;
         if let Some(v) = self.iterators[from_iterator].next() {
-            self.map.insert(v, from_iterator);
+            map.insert(v, from_iterator);
         }
-        Some(val)
+        Some((val, from_iterator))
     }
 }
 
-impl<'i, T: Ord, I: Iterator<Item = T>> MergeIters<'i, T, I> {
-    /// Merge n sorted iterators
-    pub fn new(iterators: &'i mut [I]) -> Self {
-        let mut map = BTreeMap::new();
-        for (i, iterator) in iterators.iter_mut().enumerate() {
-            if let Some(v) = iterator.next() {
-                map.insert(v, i);
+impl<T: Ord, I: Iterator<Item = T> + DoubleEndedIterator> DoubleEndedIterator for MergeIters<T, I> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        let map = if let Some(map) = &mut self.back_map {
+            map
+        } else {
+            let mut map = BTreeMap::new();
+            for (i, iterator) in self.iterators.iter_mut().enumerate() {
+                if let Some(v) = iterator.next_back() {
+                    map.insert(v, i);
+                }
             }
+            self.back_map = Some(map);
+            if let Some(map) = &mut self.back_map {
+                map
+            } else {
+                panic!()
+            }
+        };
+        let (val, from_iterator) = map.pop_last()?;
+        if let Some(v) = self.iterators[from_iterator].next_back() {
+            map.insert(v, from_iterator);
         }
-        MergeIters { iterators, map }
+        Some((val, from_iterator))
+    }
+}
+
+impl<T: Ord, I: Iterator<Item = T>> MergeIters<T, I> {
+    /// Merge n sorted iterators
+    pub fn new(iterators: Vec<I>) -> Self {
+        Self {
+            iterators,
+            front_map: None,
+            back_map: None,
+        }
     }
 }
