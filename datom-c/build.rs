bca808d9ab6668e0207a5f611f895e87352973f3
Piper McCorkle <contact@piperswe.me>
2021-08-25 21:35:02 -0500

    CI ded

--- datom-c/build.rs
+++ datom-c/build.rs
@@ -2,17 +2,18 @@
 // SPDX-License-Identifier: BlueOak-1.0.0 OR BSD-2-Clause-Patent
 // SPDX-FileContributor: Piper McCorkle <piper@lutris.engineering>
 
-use std::env;
+// use std::env;
 
 fn main() {
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let profile = env::var("PROFILE").unwrap();
+    // TODO: fix this in CI
+    // let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    // let profile = env::var("PROFILE").unwrap();
 
-    if let Ok(result) = cbindgen::Builder::new()
-        .with_crate(crate_dir)
-        .with_language(cbindgen::Language::C)
-        .generate()
-    {
-        result.write_to_file(format!("../target/{}/datom.h", profile));
-    }
+    // if let Ok(result) = cbindgen::Builder::new()
+    //     .with_crate(crate_dir)
+    //     .with_language(cbindgen::Language::C)
+    //     .generate()
+    // {
+    //     result.write_to_file(format!("../target/{}/datom.h", profile));
+    // }
 }
