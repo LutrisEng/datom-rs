2fe3dcef819877c9e84af6e32e04a0e03abd9311
Piper McCorkle <contact@piperswe.me>
2021-08-25 21:51:29 -0500

    Appease the REUSE gods

--- datom-c/run_cbindgen.sh
+++ datom-c/run_cbindgen.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2021 Lutris Engineering, Inc
+# SPDX-License-Identifier: BlueOak-1.0.0 OR BSD-2-Clause-Patent
+# SPDX-FileContributor: Piper McCorkle <piper@lutris.engineering>
 
 set -euxo pipefail
 
