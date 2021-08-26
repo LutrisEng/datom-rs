7f0e8d1aa6f956ef7304a8584ca0397c5498293e
Piper McCorkle <contact@piperswe.me>
2021-08-25 20:35:25 -0500

    Fix the in-memory backend

--- datom/tests/common/mod.rs
+++ datom/tests/common/mod.rs
@@ -2,5 +2,7 @@
 // SPDX-License-Identifier: BlueOak-1.0.0 OR BSD-2-Clause-Patent
 // SPDX-FileContributor: Piper McCorkle <piper@lutris.engineering>
 
+#[allow(dead_code)]
 pub mod data;
+#[allow(dead_code)]
 pub mod schema;
