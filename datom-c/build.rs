37d5c20ceb90c0c35b8a0696ff5cf148c99c0ccc
Piper McCorkle <contact@piperswe.me>
2021-08-25 21:28:33 -0500

    Eh it doesn't matter much if it fails

--- datom-c/build.rs
+++ datom-c/build.rs
@@ -8,10 +8,11 @@ fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let profile = env::var("PROFILE").unwrap();
 
-    cbindgen::Builder::new()
+    if let Ok(result) = cbindgen::Builder::new()
         .with_crate(crate_dir)
         .with_language(cbindgen::Language::C)
         .generate()
-        .expect("Unable to generate bindings")
-        .write_to_file(format!("../target/{}/datom.h", profile));
+    {
+        result.write_to_file(format!("../target/{}/datom.h", profile));
+    }
 }
