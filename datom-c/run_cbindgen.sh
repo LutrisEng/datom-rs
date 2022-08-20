fc90a60264e2e72865bb5db6d20018026be3e802
Piper McCorkle <contact@piperswe.me>
2022-08-19 19:44:43 -0500

    Fix copyright

--- datom-c/run_cbindgen.sh
+++ datom-c/run_cbindgen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: 2021 Lutris Engineering, Inc
+# SPDX-FileCopyrightText: 2022 Lutris, Inc
 # SPDX-License-Identifier: BlueOak-1.0.0 OR BSD-2-Clause-Patent
 # SPDX-FileContributor: Piper McCorkle <piper@lutris.engineering>
 
