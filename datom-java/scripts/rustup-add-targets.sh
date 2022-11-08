9cd586724834401dd9ef6cc34e327917b531a54e
Piper McCorkle <contact@piperswe.me>
2022-11-08 16:05:34 -0600

    Fix REUSE

--- datom-java/scripts/rustup-add-targets.sh
+++ datom-java/scripts/rustup-add-targets.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2022 Lutris, Inc
+# SPDX-License-Identifier: BlueOak-1.0.0 OR BSD-2-Clause-Patent
+# SPDX-FileContributor: Piper McCorkle <piper@lutris.engineering>
 
 set -euxo pipefail
 
